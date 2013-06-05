a9146eaae4455aac108c4429108e9067547fd55b
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2013-06-05 17:18:29 +0200

    Refactor

--- DeltaFunctor.hs
+++ DeltaFunctor.hs
@@ -5,22 +5,15 @@ import Prelude hiding ((+), (-), id, (.))
 import qualified Prelude as P
 import Control.Category hiding (id)
 import Control.Arrow
+import ArrowUtils
+
+import Delta
+
 {-
 import qualified Data.Map as M
 import Data.Map(Map, (!))
 -}
 
-import Delta
-
--- star2' :: Arrow a => (b -> a c d) -> (b' -> a c' d') -> (b, b') -> a (c, c') (d, d')
--- star2' op1 op2 = uncurry (***) . (op1 *** op2)
--- plus2 :: (Changing c, Changing c') => (c, c') -> (Delta c, Delta c') -> (c, c')
--- plus2 = (+) `star2` (+)
-
--- Yet better. This is (***) for binary functions.
-star2 :: (Arrow a1, Arrow a2) =>
- (a1 b (a2 c d)) -> (a1 b' (a2 c' d')) -> a1 (b, b') (a2 (c, c') (d, d'))
-star2 op1 op2 = arr (uncurry (***)) . (op1 *** op2)
 
 instance (Changing a, Changing b) => Changing (a, b) where
   type Delta (a, b) = (Delta a, Delta b)
